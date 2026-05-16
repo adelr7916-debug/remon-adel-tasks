969eb79cdb354a28ed04c4c02816a50a5a28bf8e
Remon adel reda <adelr7916@gmail.com>
2026-05-16 19:34:30 +0300

    Update task_3.dart

--- Remon-adel-read-main/task_3.dart
+++ Remon-adel-read-main/task_3.dart
@@ -1,11 +1,12 @@
 void main() {
   Set<String> students = {};
 
-// Function لإضافة طالب
+  // Function to add student
   void addStudent(String name) {
     students.add(name);
   }
 
+  // Recursive function to print students
   void printStudentsRecursive(List<String> list, [int index = 0]) {
     if (index >= list.length) return;
 
@@ -13,52 +14,48 @@ void main() {
     printStudentsRecursive(list, index + 1);
   }
 
-  
-  addStudent("Andrew");
   addStudent("Ali");
   addStudent("Sara");
+  addStudent("Mona");
 
   print("Students using recursion:");
   printStudentsRecursive(students.toList());
 
-  
   print("\nStudents using lambda:");
   students.forEach((student) => print(student));
 
-  
-  Set<String> newStudents = {"Mona", "Omar"};
+  Set<String> newStudents = {"Omar", "Nada"};
 
-  
   students = {...students, ...newStudents};
 
   print("\nAfter merging sets:");
-  students.forEach((s) => print(s));
+  students.forEach((student) => print(student));
 
-
-  Map<String, List<double>> studentCourses = {};
+  Map<String, List<double>> studentGrades = {};
 
   void addCourse(String student, String course, [double grade = 0]) {
-    studentCourses.putIfAbsent(student, () => []);
-    studentCourses[student]!.add(grade);
+    studentGrades.putIfAbsent(student, () => []);
+    studentGrades[student]!.add(grade);
   }
 
-  // Function لحساب متوسط الدرجات
+  // Function to calculate average grade
   double averageGrade(String student) {
-    var grades = studentCourses[student];
+    var grades = studentGrades[student];
 
     if (grades == null || grades.isEmpty) {
       return 0;
     }
 
-    double sum = grades.reduce((a, b) => a + b); // lambda
+    double sum = grades.reduce((a, b) => a + b);
+
     return sum / grades.length;
   }
 
-  // إضافة كورسات
-  addCourse("Andrew", "Math", 90);
-  addCourse("Andrew", "Programming", 95);
-  addCourse("Ali", "Math", 80);
+  // Add courses
+  addCourse("Ali", "Math", 90);
+  addCourse("Ali", "Programming", 95);
+  addCourse("Sara", "Math", 80);
 
-  print("\nAverage grade of Andrew:");
-  print(averageGrade("Andrew"));
+  print("\nAverage grade of Ali:");
+  print(averageGrade("Ali"));
 }
